cda7115b73db654f1e00d294138394b67bf63366
Jesse0502 <jas0502singh@gmail.com>
2022-05-03 13:04:07 +0530

    done

--- main.go
+++ main.go
@@ -1,19 +1,97 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"log"
-	"net/http"
 	"math/rand"
+	"net/http"
 	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
 
-func main(){
-	fmt.Println("Hello World")
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content/type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(9999))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newBooks []Book
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID != params["id"] {
+			newBooks = append(newBooks, item)
+		}
+	}
+	books = newBooks
+
+	json.NewEncoder(w).Encode("Book Deleted Succesfully")
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+
+}
+
+func main() {
+
+	books = append(books, Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
 
 	r := mux.NewRouter()
-	
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/book/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/book/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/book", createBook).Methods("POST")
+	r.HandleFunc("/api/book/{id}", deleteBook).Methods("DELETE")
+
+	fmt.Println("server listening on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
