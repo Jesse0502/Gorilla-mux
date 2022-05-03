8cf819959fb30bebead65869703416f21dd06f90
Jasmeet Singh <jasmeet.singh@softobiz.com>
2022-05-03 12:08:08 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+
+func main(){
+	fmt.Println("Hello World")
+
+	r := mux.NewRouter()
+	
+}
