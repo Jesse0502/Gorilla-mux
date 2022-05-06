50d78cdb2024ab348438bed3f1401d56816a3586
Jesse0502 <jas0502singh@gmail.com>
2022-05-06 16:21:01 +0530

    finished crud app with mux

---
+++ structs.go
@@ -0,0 +1,13 @@
+package main
+
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
